f8794279960944b7858f5ab59585f47b650b87e5
Joncalde23 <jonathancalde2010@gmail.com>
2025-11-30 22:59:43 -0500

    version 1

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import time
 import pandas as pd
 import scipy.stats
 import streamlit as st
-import time
+
 
 # estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
 if 'experiment_no' not in st.session_state:
@@ -15,6 +16,8 @@ st.header('Lanzar una moneda')
 chart = st.line_chart([0.5])
 
 def toss_coin(n):
+
+    '''lanza una moneda n veces y devuelve la media de resultados "cara" (1)'''
 
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
@@ -38,16 +41,18 @@ start_button = st.button('Ejecutar')
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
     st.session_state['experiment_no'] += 1
-    mean = toss_coin(number_of_trials)
+    promedio = toss_coin(number_of_trials)
     st.session_state['df_experiment_results'] = pd.concat([
         st.session_state['df_experiment_results'],
         pd.DataFrame(data=[[st.session_state['experiment_no'],
                             number_of_trials,
-                            mean]],
+                            promedio]],
                      columns=['no', 'iteraciones', 'media'])
         ],
         axis=0)
-    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
+    st.session_state['df_experiment_results'] = (
+        st.session_state['df_experiment_results']
+        .reset_index(drop=True))
 
 st.write(st.session_state['df_experiment_results'])
 #Version 1
